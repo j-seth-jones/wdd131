49c9bcaeb25a905664782f2e3988092cce9cde21
j-seth-jones <jsethjones2000@gmail.com>
2026-04-30 13:28:00 -0600

    renamed:    mission/byui-logo.webp -> mission/byui-logo-blue.png 	new file:   mission/byui-logo-white.png 	modified:   mission/index.html 	modified:   mission/mission.js 	modified:   mission/stylesheet.css

--- mission/mission.js
+++ mission/mission.js
@@ -5,10 +5,22 @@ let logo = document.querySelector('img');
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
-    let current = selectElem.value;
-    if (current == 'dark') {
-        // code for changes to colors and logo
+    let mode = selectElem.value;
+    let body = document.querySelector("body");
+    let hr = document.querySelector("hr");
+
+    if (mode == 'dark') {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        hr.style.borderColor = "white";
+        logo.setAttribute("src", "byui-logo-white.png");
+        logo.style.width = "150px";
+
     } else {
-        // code for changes to colors and logo
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        hr.style.borderColor = "rgba(0,0,0,.1)";
+        logo.setAttribute("src", "byui-logo-blue.png");
+        logo.style.width = "175px";
     }
 }
