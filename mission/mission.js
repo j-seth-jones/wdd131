abc6b4f3cd14bf424144759268e32bb72b071fe0
j-seth-jones <jsethjones2000@gmail.com>
2026-05-05 11:25:59 -0600

    modified:   mission/mission.js 	modified:   mission/stylesheet.css 	new file:   ponder/responsive.css 	new file:   ponder/responsive.html

--- mission/mission.js
+++ mission/mission.js
@@ -23,4 +23,41 @@ function changeTheme() {
         logo.setAttribute("src", "byui-logo-blue.png");
         logo.style.width = "175px";
     }
+
+    poofConfetti();
+}
+
+
+function poofConfetti() {
+    let container = document.querySelector("#confetti-container");
+    if (!container) {
+        container = document.createElement("div");
+        container.id = "confetti-container";
+        document.body.appendChild(container);
+    }
+
+    const colors = ["#005aff", "#ffcc00", "#ffffff", "#ff0000"];
+
+    for (let i = 0; i < 100; i++) {
+        const confetti = document.createElement("div");
+        confetti.classList.add("confetti-piece");
+        confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+        
+        const xSpread = (Math.random() - 0.5) * 100 + "vw";
+        const popHeight = "-" + (Math.random() * 40 + 60) + "vh"; 
+        const rotation = Math.random() * 1000 + "deg";
+        const duration = Math.random() * 2 + 4 + "s"; 
+
+        confetti.style.setProperty('--x-spread', xSpread);
+        confetti.style.setProperty('--pop-height', popHeight);
+        confetti.style.setProperty('--rotation', rotation);
+        
+        confetti.style.animation = `confetti-burst ${duration} ease-out forwards`;
+
+        container.appendChild(confetti);
+
+        setTimeout(() => {
+            confetti.remove();
+        }, 6000); 
+    }
 }
