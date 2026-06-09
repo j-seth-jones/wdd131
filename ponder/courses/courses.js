b79e5060c70f4c3a396332866d4357a35952b3a9
Joshua Seth Jones <jsethjones2000@gmail.com>
2026-06-09 11:23:45 -0600

    new file:   ponder/courses/courses.html 	new file:   ponder/courses/courses.js

---
+++ ponder/courses/courses.js
@@ -0,0 +1,43 @@
+// Javascript object to hold multiple sections of one course
+
+const aCourse = {
+    name: "Dynamic Web Fundamentals",
+    code: "WDD131",
+    sections: [
+        {sectionNum: 1, roomNum: "STC 231", enrolled: 12, days: "TTh", instructor: "Brother Warner"},
+        {sectionNum: 2, roomNum: "STC 347", enrolled: 0, days: "TTh", instructor: "Brother Warner"}
+    ],
+    enrollStudent: function(sectionNum) {
+        //sectionNum represents the section the user wants to add student to
+        // find that section in our array, add a student to it
+        this.sections.forEach(function(section) {
+            if (section.sectionNum == sectionNum) {
+                section.enrolled++;
+            }
+        });
+        renderSections(this.sections);
+
+        //alternative
+        //this.section.find(section => section.sectionNum == sectionNum).enrolled++;
+    }
+}
+    function sectionTemplate(section) {
+    return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+const html = sections.map(sectionTemplate);
+document.querySelector("#sections").innerHTML = html.join("");
+}
+
+renderSections(aCourse.sections);
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
